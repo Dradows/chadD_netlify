a1e941e484abfb37ef971df5c875b9cc75f46e8f
Blaston <1023086065@qq.com>
2023-06-07 16:05:16 +0800

    fix typo

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  await fetch('https://chatd-production.up.railway.app/api/chkeck', {
+  await fetch('https://chatd-production.up.railway.app/api/check', {
     method: 'POST',
     body: req.body,
   });
