a4622caa924b42658e48d4b3fd2b8affa339f79a
Blaston <1023086065@qq.com>
2023-06-07 15:54:18 +0800

    fix

--- pages/api/chatgpt.js
+++ pages/api/chatgpt.js
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  const prompt = JSON.parse(req.body);
-  const text = await fetch('https://chatd-production.up.railway.app/api/chatgpt', {
-    method: 'POST',
-    body: JSON.stringify({
-      prompt: prompt,
-    }),
-  });
-  const json = await text.json()
+  // const prompt = JSON.parse(req.body);
+  const text = await fetch(
+    'https://chatd-production.up.railway.app/api/chatgpt',
+    {
+      method: 'POST',
+      body: req.body,
+    }
+  );
+  const json = await text.json();
   res.status(200).json(json);
 }
